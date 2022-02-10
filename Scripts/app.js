96cfe2cf059c3f30f225a4708d96b85f3cf8c8e5
Jashakan Shanmugathasan <jashakan2000@gmail.com>
2022-02-09 21:47:36 -0500

    updated ICE-2

--- Scripts/app.js
+++ Scripts/app.js
@@ -10,6 +10,40 @@
         {
             location.href = "about.html"
         });
+
+        // Step 1 - get a reference to an entry pont(s) (insertion/deletion)
+        let MainContent = document.getElementsByTagName("main")[0];
+        let DocumentBody = document.body;
+
+        // Step 2 - create a HTML element in memory
+        let MainParagraph = document.createElement("p");
+        let Article = document.createElement("article");
+        let ArticleParagraph = `<p id="ArticlParagraph" class="mt-3">This is the Article Paragraph</p>`;
+
+        // Step 3 - Configure new Element
+        MainParagraph.setAttribute("id", "MainParagraph");
+        MainParagraph.textContent = "This is the Main Paragraph";
+        let FirstString = "This is";
+        let SecondString = `${FirstString} the Main Paragraph`;
+        MainParagraph.textContent = SecondString;
+        Article.setAttribute("class", "container");
+        
+        
+        // Step 4 - Perform insertion / deletion
+
+        // example of insert after (append)
+        MainContent.appendChild(MainParagraph);
+        Article.innerHTML = ArticleParagraph;
+        DocumentBody.appendChild(Article);
+
+        // example of insert before
+        //MainContent.before(MainParagraph);
+
+        // example of deletion
+        //document.getElementById("AboutUsButton").remove();
+        //AboutUsButton.remove();
+
+        // ES6 and HTML5 => Template strings => "Super strings"
     }
 
     function DisplayProductsPage()
